166cb848296e5b5ea031ace44b90b63277f8023c
Omar Labana <omarlabana@outlook.com>
2021-06-29 00:18:44 +0200

    Enforce user_id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, only: %i[edit update show destory]
+  before_action :authenticate, only: %i[show edit]
 
   # GET /posts or /posts.json
   def index
@@ -22,7 +23,8 @@ class PostsController < ApplicationController
   # POST /posts or /posts.json
   def create
     @post = Post.new(post_params)
-
+    @post.user_id = current_user.id
+    
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: "Post was successfully created." }
